ba8d42f68e74fdfb4967a62563cd7f2d6e25a57e
hamidwise1 <hrawebdev@gmail.com>
2023-12-24 15:56:21 +0330

    add socket connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import { createServer } from 'http';
 import 'express-async-errors';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import { notFoundMiddleware } from './middlewares/notFound.js';
+import { Server } from 'socket.io';
 
 dotenv.config();
 const app = express();
-
+const server = createServer(app);
+// * socket io setup
+const io = new Server(server);
 // * basic middlewares setup
 process.env.ENVIROMENT == 'DEVELOPMENT' && app.use(morgan('dev'));
 app.use(express.static('public'));
@@ -14,10 +18,22 @@ app.use(express.json());
 
 // * api base route
 const apiBaseRoute = '/api/v1';
+
+// * socket io connections
+io.on('connection', (soket) => {
+ console.log(soket);
+ console.log('user connected');
+ soket.on('disconnet', () => {
+  console.log('user disconnected');
+ });
+ soket.on('chat message', (e) => {
+  console.log('chat begin', e);
+ });
+});
 // * advanced middlewares
 app.all('*', notFoundMiddleware);
 // * server initialization
 const serverPort = process.env.PORT;
-app.listen(serverPort, () => {
+server.listen(serverPort, () => {
  console.log(`server is listening to port:${serverPort}`);
 });
